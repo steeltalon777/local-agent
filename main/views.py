e1cbecc45b44351a84b68f99c80071eca7ef95e7
Stonecold14 <avatarmakc@yandex.ru>
2026-02-07 13:03:05 +0900

    базовый проект готов

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home_page(request):
+    return HttpResponse("""
+        <h1>🎉 Мой первый Django проект работает!</h1>
+        <p>Привет от приложения 'main'!</p>
+        <p><a href="/admin/">Админка Django</a></p>
+    """)
+
+
